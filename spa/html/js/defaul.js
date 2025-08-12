1224d6b1905451c9c7fdd65694162d0c0d7a6507
unknown <kidzmonster2k@gmail.com>
2025-08-12 09:45:17 +0700

    Cam on anh em

---
+++ spa/html/js/defaul.js
@@ -0,0 +1,14 @@
+
+
+	
+function showmenu()
+		{
+			if($(".menu-top").css('display')=='none')
+			{
+				$(".menu-top").fadeIn("fast");
+			}
+			else
+			{
+				$(".menu-top").fadeOut("fast");
+			}
+	}	
